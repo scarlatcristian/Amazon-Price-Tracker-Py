a33a772a212d631542659615fa9aa7e310d3549c
cristi <scarlat.cristian.gabriel@gmail.com>
2023-12-03 00:37:23 +0200

    added csv to check prices for more products

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import requests
 import smtplib
+import pandas
 from bs4 import BeautifulSoup
 
 my_email = 'YOUR_EMAIL'
 my_password = 'YOUR_PASSWORD'
 
-url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
-res = requests.get(url=url)
+data = pandas.read_csv("products.csv").to_dict()
 
-soup = BeautifulSoup(res.text, "html.parser")
-product_name = soup.select_one(
-    ".product-title-word-break").get_text().split(",")[0].lstrip(" ")
-product_price = soup.select_one(".a-offscreen").get_text().split("$")[1]
+for key, value in data["amazon_links"].items():
+    url = value
+    price_to_buy = data["price_to_buy"][key]
+    res = requests.get(url=url)
 
-print(product_name)
-print(float(product_price))
+    soup = BeautifulSoup(res.text, "html.parser")
+    product_price = soup.select_one(".a-offscreen").get_text().split("$")[1]
 
-
-if float(product_price) < 100:
-    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=my_password)
-        connection.sendmail(from_addr=my_email, to_addrs=my_email,
-                            msg=f"Subject:Price for {product_name}\n\nThe current price for {product_name} is only {product_price}$")
+    if float(product_price) <= price_to_buy:
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(from_addr=my_email, to_addrs=my_email,
+                                msg=f"Subject:Price for {data['product_name'][key]}\n\nThe current price for {data['product_name'][key]} is only {product_price}$")
